05900beacc0eaabcbb25428590c94cc1f7865478
XUEYING <32117886+raphealshirley@users.noreply.github.com>
2018-07-07 13:04:07 -0400

    scaffolded

---
+++ ShoppingCart/DAL/BookInitializer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShoppingCart.Models;
+using System.Data.Entity;
+
+namespace ShoppingCart.DAL
+{
+    public class BookInitializer:DropCreateDatabaseIfModelChanges<BookContext>
+    {
+        protected override void Seed(BookContext context)
+        {
+            var author = new Author
+            {
+                Biography = "...",
+                FirstName = "Jamie",
+                LastName = "Munro"
+            };
+
+            var books = new List<Book>
+            {
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl = "http://ecx.images-amazon.com/images/I/51T%2BWt430bL._AA160_.jpg",
+                    Isbn = "11111111",
+                    Synopsis = "...",
+                    Title = "aaaaa"
+                },
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl = "http://ecx.images-amazon.com/images/I/51T%2BWt430bL._AA160_.jpg",
+                    Isbn = "22222222",
+                    Synopsis = "...",
+                    Title = "bbbbb"
+                },
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl = "http://ecx.images-amazon.com/images/I/51T%2BWt430bL._AA160_.jpg",
+                    Isbn = "333333333",
+                    Synopsis = "...",
+                    Title = "cccccccc"
+                },
+                new Book
+                {
+                    Author = author,
+                    Description = "...",
+                    ImageUrl = "http://ecx.images-amazon.com/images/I/51T%2BWt430bL._AA160_.jpg",
+                    Isbn = "4444444444",
+                    Synopsis = "...",
+                    Title = "dddddddd"
+                }
+            };
+            books.ForEach(b => context.Books.Add(b));
+            context.SaveChanges();
+                
+            }
+        }
+   }
--- ShoppingCart/Global.asax.cs
+++ ShoppingCart/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using ShoppingCart.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Data.Entity;
 
 namespace ShoppingCart
 {
@@ -16,6 +18,11 @@ namespace ShoppingCart
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var bookContext = new BookContext();
+            Database.SetInitializer(new BookInitializer());
+            bookContext.Database.Initialize(true);
+
         }
     }
 }
